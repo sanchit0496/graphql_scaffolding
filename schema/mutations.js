e123c41657a9612a9a71f75c78f2555fd1629694
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-01-15 00:03:20 +0530

    CRUD added on user

--- schema/mutations.js
+++ schema/mutations.js
@@ -1,23 +1,64 @@
-const { GraphQLObjectType, GraphQLString } = require('graphql');
+// schema/mutations.js
+const { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLInt } = require('graphql');
 const UserType = require('./types/UserType');
 const User = require('../models/User');
 
-const UserMutations = new GraphQLObjectType({
+const addUser = {
+  type: UserType,
+  args: {
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    email: { type: new GraphQLNonNull(GraphQLString) },
+  },
+  resolve(parent, args) {
+    return User.create({
+      name: args.name,
+      email: args.email
+    });
+  },
+};
+
+const updateUser = {
+  type: UserType,
+  args: {
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+  },
+  resolve(parent, args) {
+    return User.findByPk(args.id).then(user => {
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return user.update({
+        name: args.name || user.name,
+        email: args.email || user.email
+      });
+    });
+  },
+};
+
+const deleteUser = {
+  type: UserType,
+  args: {
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+  },
+  resolve(parent, args) {
+    return User.findByPk(args.id).then(user => {
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return user.destroy();
+    });
+  },
+};
+
+const Mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    addUser: {
-      type: UserType,
-      args: {
-        name: { type: GraphQLString },
-        email: { type: GraphQLString },
-        // Define other arguments
-      },
-      resolve(_, args) {
-        return User.create(args);
-      },
-    },
-    // Define other mutation fields
-  },
+    addUser: addUser,
+    updateUser: updateUser,
+    deleteUser: deleteUser
+  }
 });
 
-module.exports = UserMutations
+module.exports = Mutation;
