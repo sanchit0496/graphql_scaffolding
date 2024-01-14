7c06201e930be2841085f32d2cb65ab47a0037df
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-01-15 02:36:08 +0530

    adding middleware for logging purpose

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
-const sequelize = require('./config/database');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const sequelize = require("./config/database");
+const logger = require("./logger"); // Make sure the path is correct
+const { applyMiddleware } = require('graphql-middleware');
 
 const app = express();
 
-
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true, // Enable GraphiQL in development
-}));
+const loggingMiddleware = (resolve, root, args, context, info) => {
+    const { fieldName, returnType } = info;
+    console.log('info', info)
+    logger.info(`Query/Mutation: ${fieldName}, Return Type: ${returnType}`);
+    console.log('inside middleware')
+    try {
+      const result = resolve(root, args, context, info);
   
-app.listen(3000, () => console.log('Server running on port 3000'));
+      if (result instanceof Promise) {
+        return result.catch(err => {
+          logger.error(`Error in ${fieldName}: ${err}`);
+          throw err;
+        });
+      }
+  
+      return result;
+    } catch (err) {
+      logger.error(`Error in ${fieldName}: ${err}`);
+      throw err;
+    }
+  };
+  
+
+const schemaWithMiddleware = applyMiddleware(schema, loggingMiddleware);
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schemaWithMiddleware,
+    graphiql: true, // Enable GraphiQL in development
+  })
+);
+
+app.listen(3000, () => console.log("Server running on port 3000"));
